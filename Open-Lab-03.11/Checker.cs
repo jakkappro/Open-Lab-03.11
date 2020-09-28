adf531e5c55f24b6d61f8faf1164a835dfdc8948
jakkappro <jakkappro@gmail.com>
2020-09-28 09:48:59 +0200

    I made function to reverse string and comare it fith original

--- Open-Lab-03.11/Checker.cs
+++ Open-Lab-03.11/Checker.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Open_Lab_03._11
 {
@@ -6,7 +7,21 @@ namespace Open_Lab_03._11
     {
         public bool IsPalindrome(string str)
         {
-            throw new NotImplementedException();
+            char[] originalStr = str.ToCharArray();
+            string reverseStr = "";
+            
+            for(int i = originalStr.Length - 1; i >= 0; i--)
+            {
+                reverseStr += originalStr[i];
+            }
+
+
+            if(str == reverseStr)
+            {
+                return true;
+            }
+
+            return false;
         } 
     }
 }
